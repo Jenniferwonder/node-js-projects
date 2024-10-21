b1b4b1d65b4306099dc562aece0f0fd512d59c98
Jenniferwonder <Jenniferwonder@163.com>
2024-10-21 21:23:33 +0800

    feat: add CRUD operations using mongoose

--- natours-user/controllers/2-tourController-mongo.js
+++ natours-user/controllers/2-tourController-mongo.js
@@ -3,19 +3,57 @@ const Tour = require('../modals/tourModal');
 // Route Handlers
 
 // ✅ READ DATA
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: { tours },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-  });
+
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    // Tour.findOne({_id:req.params.id})
+
+    res.status(200).json({
+      status: 'success',
+      data: tour,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 // ✅ UPDATE DATA
-exports.updateTour = (req, res) => {};
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: tour,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
 
 // ✅ CREATE DATA
 /* exports.createTour = (req, res) => {
@@ -44,9 +82,19 @@ exports.createTour = async (req, res) => {
 };
 
 // ✅ DELETE DATA
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id, {
+      new: true,
+    });
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
